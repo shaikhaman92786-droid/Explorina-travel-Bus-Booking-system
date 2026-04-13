0c784696840784dec76a47078d9045e5d05d1668
Shaikh Aman <shaikhaman92786@gmail.com>
2026-04-13 13:41:03 +0530

    compelte

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: `${import.meta.env.REACT_API_BACKEND_BASEURL}`
+    baseURL: `${import.meta.env.VITE_REACT_API_BACKEND_BASEURL}`
 })
 
 instance.interceptors.request.use((config) => {
